be0143477fac3e3289de3333d8426e3766344d5c
Ear <tosiear@gmail.com>
2022-01-29 15:37:56 +0200

    uichanges

--- Prefabs/PrefabScripts/CharacterDisplay.gd
+++ Prefabs/PrefabScripts/CharacterDisplay.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
+export(Texture) var icon_25
+export(Texture) var icon_50
+export(Texture) var icon_75
+export(Texture) var icon_100
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var hp = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,5 +13,17 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	#get hp
+	$CharacterIcon.texture = icon_25
+	if (hp > 25):
+		$CharacterIcon.texture = icon_50
+	if (hp > 55):
+		$CharacterIcon.texture = icon_75
+	if (hp > 75):
+		$CharacterIcon.texture = icon_100
+	
+
+func get_hp():
+	return hp #placeholder
+	
--- Prefabs/PrefabScripts/StatusIcon.gd
+++ Prefabs/PrefabScripts/StatusIcon.gd
@@ -1,13 +1,10 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+export(Texture) var icon
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Icon.texture = icon
 	pass # Replace with function body.
 
 
