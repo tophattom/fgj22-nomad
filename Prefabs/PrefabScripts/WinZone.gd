24f310f628192072973377ac009c0d8e0513b875
Ear <tosiear@gmail.com>
2022-01-30 14:09:31 +0200

    winzone

---
+++ Prefabs/PrefabScripts/WinZone.gd
@@ -0,0 +1,23 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$WinScreen.hide()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area_body_entered(body):
+	if body.name != "CharacterGroup":
+		return
+	$WinScreen.show()
